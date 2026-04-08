918ffc3d1a0fd0ee0c6b3376ef0e597131cfee16
Charlie Lashmar <c.lashmar08@gmail.com>
2026-04-08 23:04:09 +0100

    fix(build): use brew to install cargo make

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -5,6 +5,6 @@ echo "==> Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 echo "==> Installing cargo-make..."
-cargo install cargo-make
+brew install cargo-make
 
 echo "==> Setup complete!"
